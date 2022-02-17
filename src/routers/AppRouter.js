2e0c5b455a43586a9b7c1a8310fc1555556ea805
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-17 18:23:08 -0300

    Pantalla de carga y usuarioLogueado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     BrowserRouter,
     Route,
@@ -15,15 +15,44 @@ export const AppRouter = () => {
 
     const dispatch = useDispatch();
 
+    const [checking, setChecking] = useState(true)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
     useEffect(() => {
         onAuthStateChanged(getAuth(), user => {
             //console.log(user)
             if(user?.uid) {
                 dispatch(login(user.uid, user.displayName))
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(true)
             }
+
+            setChecking(false)
         })
         
     }, [dispatch]);
+
+    if(checking) {
+        return (
+            <div className="auth__main">
+                <div className="sk-fading-circle">
+                    <div className="sk-circle1 sk-circle"></div>
+                    <div className="sk-circle2 sk-circle"></div>
+                    <div className="sk-circle3 sk-circle"></div>
+                    <div className="sk-circle4 sk-circle"></div>
+                    <div className="sk-circle5 sk-circle"></div>
+                    <div className="sk-circle6 sk-circle"></div>
+                    <div className="sk-circle7 sk-circle"></div>
+                    <div className="sk-circle8 sk-circle"></div>
+                    <div className="sk-circle9 sk-circle"></div>
+                    <div className="sk-circle10 sk-circle"></div>
+                    <div className="sk-circle11 sk-circle"></div>
+                    <div className="sk-circle12 sk-circle"></div>
+                </div>
+            </div>
+        )
+    }
 
 
     return (
