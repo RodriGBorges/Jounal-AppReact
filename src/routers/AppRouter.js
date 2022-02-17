6c942be1ba4e5456077703d054522b454669b9c1
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-17 18:07:57 -0300

    Recordar usuario logueado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter,
     Route,
@@ -7,8 +7,25 @@ import {
 
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        onAuthStateChanged(getAuth(), user => {
+            //console.log(user)
+            if(user?.uid) {
+                dispatch(login(user.uid, user.displayName))
+            }
+        })
+        
+    }, [dispatch]);
+
+
     return (
         <BrowserRouter>
             <div>
