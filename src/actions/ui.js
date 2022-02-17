2f68ef05a88b8ff9c1f87e2df92c13f47a5b0ad4
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-17 16:52:25 -0300

    Se crea los tipos cargando y carga terminada

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -3,8 +3,16 @@ import { types } from "../types";
 export const setError = (error) => ({
     type: types.uiSetError,
     payload: error
-})
+});
 
 export const removeError = () => ({
     type: types.uiRemoveError
+});
+
+export const startLoading = () => ({
+    type: types.uiStartLoading
+});
+
+export const finishLoading = () => ({
+    type: types.uiFinishLoading
 })
