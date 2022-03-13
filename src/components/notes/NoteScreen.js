5be4eaf2639715808c0408fa56e5697a72606eb3
RodriGBorges <gabii.borges1998@gmail.com>
2022-03-13 17:11:15 -0300

    Switch de notas, nota activa

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
@@ -8,9 +8,21 @@ export const NoteScreen = () => {
     const { active: note} = useSelector(state => state.notes);
     // console.log(note);
 
+    const activeId = useRef(note.id);
+
     const [formValues, handleInputChange, reset] = useForm(note);
 
     const {title, body} = formValues;
+
+    useEffect(() => {
+
+        if(note.id !== activeId.current) {
+            reset(note)
+            activeId.current = note.id
+        }
+
+    }, [note, reset])
+    
 
     return (
         <div className="notes__main-content">
@@ -21,16 +33,20 @@ export const NoteScreen = () => {
 
                 <input 
                     type="text"
+                    name='title'
                     placeholder="Escribe el título"
                     className="notes__title-input"
                     autoComplete="off"
                     value={title}
+                    onChange={handleInputChange}
                 />
 
                 <textarea
+                    name='body'
                     placeholder="¿Qué pasó hoy?"
                     className="notes__textarea"
                     value={body}
+                    onChange={handleInputChange}
                 ></textarea>
 
                 <div className="notes__image">
