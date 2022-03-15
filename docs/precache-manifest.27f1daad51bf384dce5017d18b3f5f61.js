3c50cb27b20bd77c04ac0844cef528e3f653f329
RodriGBorges <gabii.borges1998@gmail.com>
2022-03-14 21:35:31 -0300

    Se crea build-docs

---
+++ docs/precache-manifest.27f1daad51bf384dce5017d18b3f5f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875b8ec9e0dce4dd7ee0d11d28ba390c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96011aacbb7a6995d28d",
+    "url": "/static/css/main.64038f19.chunk.css"
+  },
+  {
+    "revision": "7c51c7e1c870d7e7b5ef",
+    "url": "/static/js/2.3718ff88.chunk.js"
+  },
+  {
+    "revision": "7543a77fcf9eb195da628ccb7185b066",
+    "url": "/static/js/2.3718ff88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96011aacbb7a6995d28d",
+    "url": "/static/js/main.dba563c4.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);
